d0199169fb4272c0dc39499a79ba5ceada88e4eb
Andrey Pakhomov <stonewalrusfan@gmail.com>
2023-01-29 00:06:27 +0300

    fix stylelint config

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -15,7 +15,6 @@ module.exports = {
     ],
     rules: {
         'at-rule-disallowed-list': ['document', 'viewport', 'counter-style', 'font-feature-values'],
-
         'at-rule-empty-line-before': [
             'always',
             {
@@ -28,40 +27,24 @@ module.exports = {
                 ],
             },
         ],
-
         'at-rule-name-case': 'lower',
-
         'at-rule-name-space-after': 'always',
-
-        // Supported features
         'at-rule-no-unknown': null,
-
         'at-rule-no-vendor-prefix': true,
-
         'at-rule-semicolon-newline-after': 'always',
-
         'at-rule-semicolon-space-before': 'never',
 
         'block-closing-brace-empty-line-before': 'never',
-
         'block-closing-brace-newline-after': 'always',
-
         'block-closing-brace-newline-before': 'always-multi-line',
-
         'block-no-empty': true,
-
         'block-opening-brace-newline-after': 'always-multi-line',
-
         'block-opening-brace-space-after': 'always-single-line',
-
         'block-opening-brace-space-before': 'always',
 
         'color-hex-case': 'lower',
-
         'color-named': 'never',
-
         'color-no-invalid-hex': true,
-
         'comment-empty-line-before': [
             'always',
             {
@@ -69,17 +52,12 @@ module.exports = {
                 ignore: ['after-comment', 'stylelint-commands'],
             },
         ],
-
         'comment-no-empty': true,
-
         'comment-whitespace-inside': 'always',
-
         'comment-word-disallowed-list': ['/^TODO:/', '/^@TODO:/'],
-
         'csstree/validator': {
             syntaxExtensions: ['sass', 'less'],
         },
-
         'custom-property-empty-line-before': [
             'always',
             {
@@ -89,62 +67,39 @@ module.exports = {
         ],
 
         'declaration-bang-space-after': 'always',
-
         'declaration-bang-space-before': 'always',
-
-        // Invalid code
         'declaration-block-no-duplicate-properties': [
             true,
             {
                 ignoreProperties: ['/background\\-/'],
             },
         ],
-
         'declaration-block-no-redundant-longhand-properties': true,
-
         'declaration-block-no-shorthand-property-overrides': true,
-
         'declaration-block-semicolon-newline-after': 'always',
-
         'declaration-block-semicolon-newline-before': 'never-multi-line',
-
         'declaration-block-semicolon-space-after': 'always-single-line',
-
         'declaration-block-semicolon-space-before': 'never',
-
         'declaration-block-single-line-max-declarations': 1,
-
         'declaration-block-trailing-semicolon': 'always',
-
         'declaration-colon-space-after': 'always',
-
         'declaration-colon-space-before': 'never',
-
         'declaration-empty-line-before': [
             'never',
             {
                 ignore: ['after-comment', 'after-declaration'],
             },
         ],
-
         'declaration-no-important': true,
 
         'font-family-name-quotes': 'always-where-recommended',
-
         'font-family-no-duplicate-names': true,
-
         'font-family-no-missing-generic-family-keyword': true,
-
         'function-calc-no-unspaced-operator': true,
-
         'function-comma-newline-after': 'never-multi-line',
-
         'function-comma-newline-before': 'never-multi-line',
-
         'function-comma-space-after': 'always',
-
         'function-comma-space-before': 'never',
-
         'function-disallowed-list': [
             'hsl',
             'hsla',
@@ -160,32 +115,20 @@ module.exports = {
             'symbols',
             'translate3d',
         ],
-
         'function-linear-gradient-no-nonstandard-direction': true,
-
         'function-max-empty-lines': 0,
-
         'function-name-case': 'lower',
-
         'function-parentheses-newline-inside': 'never-multi-line',
-
         'function-parentheses-space-inside': 'never',
-
         'function-url-no-scheme-relative': true,
-
         'function-url-quotes': 'always',
-
         'function-url-scheme-disallowed-list': ['data', 'ftp', 'file', '/^http/'],
-
         'function-whitespace-after': 'always',
 
-        // Indentation style
         indentation: 4,
-
         'keyframe-declaration-no-important': true,
 
         'max-empty-lines': 1,
-
         'max-nesting-depth': [
             5,
             {
@@ -194,11 +137,8 @@ module.exports = {
         ],
 
         'media-feature-colon-space-after': 'always',
-
         'media-feature-colon-space-before': 'never',
-
         'media-feature-name-case': 'lower',
-
         'media-feature-name-disallowed-list': [
             'any-hover',
             'any-pointer',
@@ -211,51 +151,28 @@ module.exports = {
             'scripting',
             'update',
         ],
-
         'media-feature-name-no-unknown': true,
-
         'media-feature-name-no-vendor-prefix': true,
-
         'media-feature-parentheses-space-inside': 'never',
-
         'media-feature-range-operator-space-after': 'always',
-
         'media-feature-range-operator-space-before': 'always',
-
         'media-query-list-comma-newline-after': 'never-multi-line',
-
         'media-query-list-comma-newline-before': 'never-multi-line',
-
         'media-query-list-comma-space-after': 'always',
-
         'media-query-list-comma-space-before': 'never',
 
         'no-duplicate-at-import-rules': true,
-
         'no-duplicate-selectors': true,
-
-        // TODO: remove
-        'no-empty-source': true,
-
         'no-eol-whitespace': true,
-
         'no-extra-semicolons': true,
-
         'no-invalid-double-slash-comments': true,
-
         'no-missing-end-of-source-newline': true,
-
         'number-leading-zero': 'always',
-
         'number-max-precision': 2,
-
         'number-no-trailing-zeros': true,
 
-        // Code style
         'order/properties-order': [],
-
         'plugin/declaration-block-no-ignored-properties': true,
-
         'plugin/rational-order': [
             true,
             {
@@ -265,7 +182,6 @@ module.exports = {
                 severity: 'warning',
             },
         ],
-
         'plugin/z-index-value-constraint': [
             {
                 max: 10,
@@ -276,11 +192,8 @@ module.exports = {
             },
         ],
 
-        // System
         'prettier/prettier': true,
-
         'property-case': 'lower',
-
         'property-disallowed-list': [
             'property-blacklist',
             'box-decoration-break',
@@ -298,16 +211,13 @@ module.exports = {
             'perspective-origin',
             'text-combine-upright',
         ],
-
         'property-no-unknown': true,
-
         'property-no-vendor-prefix': [
             true,
             {
                 ignoreProperties: ['line-clamp', 'box-orient', 'tap-highlight-color'],
             },
         ],
-
         'rule-empty-line-before': [
             'always',
             {
@@ -317,13 +227,9 @@ module.exports = {
         ],
 
         'selector-attribute-brackets-space-inside': 'never',
-
         'selector-attribute-operator-space-after': 'never',
-
         'selector-attribute-operator-space-before': 'never',
-
         'selector-attribute-quotes': 'always',
-
         'selector-class-pattern': [
             '^.[a-z\\d-]+(?:__[a-z\\d-]+)?(?:_[a-z\\d-]+)?$',
             {
@@ -394,6 +300,7 @@ module.exports = {
         'selector-type-no-unknown': true,
         'string-no-newline': true,
         'string-quotes': 'single',
+
         'time-min-milliseconds': 100,
         'unit-case': 'lower',
         'unit-disallowed-list': ['ex', 'cm', 'mm', 'in', 'pt', 'pc', 'ch'],
--- typed-scss-modules.config.js
+++
@@ -1,9 +0,0 @@
-const { resolve } = require('path');
-
-export const config = {
-    exportType: 'default',
-    exportTypeName: 'ClassesType',
-    outputFolder: './lib',
-    aliasPrefixes: { $styles: resolve(__dirname, './src/styles') },
-    ignore: ['**/__mocks__/**', '**/__tests__/**', '**/stories/**'],
-};
