d365190cdb0aa7893b1c090fd676cade9ac24884
Andrey Pakhomov <stonewalrusfan@gmail.com>
2024-02-02 10:20:26 +0300

    Add typed-scss-module

---
+++ typed-scss-modules.config.js
@@ -0,0 +1,7 @@
+export const config = {
+    exportType: 'default',
+    exportTypeName: 'ClassesType',
+    outputFolder: './lib',
+    logLevel: 'error',
+    ignore: ['**/__mocks__/**', '**/__tests__/**', '**/stories/**'],
+};
