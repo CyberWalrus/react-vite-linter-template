d4e8eeb71a55f6f664f6750f602ed794088de0b5
Andrey Pakhomov <stonewalrusfan@gmail.com>
2024-02-22 22:00:21 +0300

    update create-store

--- src/shared/model/create-store.ts
+++ src/shared/model/create-store.ts
@@ -4,14 +4,7 @@ import { devtools } from 'zustand/middleware';
 import { createEqualityStore } from '$shared/model/create-equality-store';
 
 export const createStore = <GState>(fn: StateCreator<GState>, name: string) => {
-    if (process.env.NODE_ENV === 'development') {
-        const store = create(devtools(fn, { name }));
-        const selectedStore = createEqualityStore(store);
-
-        return [store, selectedStore];
-    }
-
-    const store = create(fn);
+    const store = process.env.NODE_ENV === 'development' ? create(devtools(fn, { name })) : create(fn);
 
     const selectedStore = createEqualityStore(store);
 
