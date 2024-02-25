8a03e1c87ff65578bf69dfed28cc4c67507cea0e
Andrey Pakhomov <stonewalrusfan@gmail.com>
2024-02-25 16:00:23 +0300

    update e2e test

--- src/app/__e2e__/counter.test.ts
+++ src/app/__e2e__/counter.test.ts
@@ -1,21 +1,30 @@
 import { expect, test } from '@playwright/test';
 
-test('counter full test', async ({ page }) => {
+test.beforeEach('Open start URL', async ({ page }) => {
     await page.goto('/');
     await page.waitForSelector('#app-start');
-    await expect(page.getByTestId('counter-value')).toHaveText('0');
-    await page.getByTestId('counter-increment').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('1');
-    await page.getByTestId('counter-increment').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('2');
-    await page.getByTestId('counter-increment').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('3');
-    await page.getByTestId('counter-increment').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('4');
-    await page.getByTestId('counter-decrement').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('3');
-    await page.getByTestId('counter-decrement').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('2');
-    await page.getByTestId('counter-clear').click();
-    await expect(page.getByTestId('counter-value')).toHaveText('0');
+});
+
+test.describe('counter', () => {
+    test('value', async ({ page }) => {
+        await expect(page.getByTestId('counter-value')).toHaveText('0');
+    });
+
+    test('increment', async ({ page }) => {
+        await page.getByTestId('counter-increment').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('1');
+        await page.getByTestId('counter-increment').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('2');
+        await page.getByTestId('counter-clear').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('0');
+    });
+
+    test('decrement', async ({ page }) => {
+        await page.getByTestId('counter-decrement').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('-1');
+        await page.getByTestId('counter-decrement').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('-2');
+        await page.getByTestId('counter-clear').click();
+        await expect(page.getByTestId('counter-value')).toHaveText('0');
+    });
 });
