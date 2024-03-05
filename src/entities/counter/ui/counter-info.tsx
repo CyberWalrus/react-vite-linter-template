9fcf492a75acab9f74231a16e472d1924a068319
Пахомов Андрей Николаевич <andrey.pahomov@ligastavok.ru>
2024-03-05 18:49:31 +0300

    reset

--- src/entities/counter/ui/counter-info.tsx
+++ src/entities/counter/ui/counter-info.tsx
@@ -1,20 +1,9 @@
-import { useEffect } from 'react';
 import { Text } from '@radix-ui/themes';
 
 import { useCounterStore } from '../model/store';
 
 export const CounterInfo = () => {
     const counter = useCounterStore(({ value }) => value);
-
-    useEffect(() => {
-        const id = setInterval(() => {
-            console.log(window.screen);
-        }, 2000);
-
-        return () => {
-            clearInterval(id);
-        };
-    }, []);
 
     return (
         <Text
