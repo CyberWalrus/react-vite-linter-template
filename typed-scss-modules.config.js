63347e126df6b3a08a68791d8d3712a90b72976b
Пахомов Андрей Николаевич <andrey.pahomov@ligastavok.ru>
2024-05-27 00:34:20 +0300

    chore: update package

--- typed-scss-modules.config.js
+++ typed-scss-modules.config.js
@@ -1,6 +1,4 @@
 export const config = {
-    exportType: 'default',
-    exportTypeName: 'ClassesType',
     outputFolder: './__generated__',
     logLevel: 'error',
     ignore: ['**/__mocks__/**', '**/__tests__/**', '**/stories/**'],
