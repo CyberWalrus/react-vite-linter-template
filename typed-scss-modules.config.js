7b9a803c84a43575bc58a0e99ddcedc54e85cfaf
Пахомов Андрей Николаевич <andrey.pahomov@ligastavok.ru>
2024-06-11 22:05:35 +0300

    chore: update some

--- typed-scss-modules.config.js
+++ typed-scss-modules.config.js
@@ -1,4 +1,9 @@
+import { resolve } from 'path';
+
 export const config = {
+    aliasPrefixes: {
+        $mixins: resolve(__dirname, './src/shared/styles/mixins.scss'),
+    },
     outputFolder: './__generated__',
     logLevel: 'error',
     ignore: ['**/__mocks__/**', '**/__tests__/**', '**/stories/**'],
